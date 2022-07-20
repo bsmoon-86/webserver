97471e9f1b0b4632193869a3ab131c3006b1df79
bsmoon-86 <ekflafg@naver.com>
2022-07-20 17:32:21 +0900

    220720_update

--- 실습/220719/index.js
+++ 실습/220719/index.js
@@ -1,11 +1,23 @@
 var express = require("express")
 var app = express()
+require('dotenv').config()
+
+var session = require('express-session')
+app.use(
+    session({
+        secret : process.env.session,
+        resave: false, 
+        saveUninitialized: true, 
+        maxAge : 36000000
+    })
+)
 
 app.set("views", __dirname+"/views")
 app.set('view engine', 'ejs')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+app.use(express.static(__dirname+"/public"))
 
 //kalytn 설정
 var Caver = require("caver-js")
@@ -40,11 +52,50 @@ async function balanceOf(address){
     return receipt
 }
 
-
+//mysql 세팅
+var mysql = require('mysql2')
+var connection = mysql.createConnection({
+    host : process.env.host, 
+    port : process.env.port,
+    user : process.env.user, 
+    password : process.env.password, 
+    database : process.env.database
+})
 
 
 app.get("/", function(req, res){
     res.render("main.ejs")
+})
+
+app.get("/main", function(req, res){
+    if(!req.session.login){
+        res.redirect("/")
+    }else{
+        res.render("index.ejs")
+    }
+})
+
+app.post("/signin", function(req, res){
+    var id = req.body._id
+    var pass = req.body._pass
+    console.log(id, pass)
+    connection.query(
+        `select * from user_list where ID = ? AND password = ?`,
+        [id, pass], 
+        function(err, result){
+            if(err){
+                console.log(err)
+                res.send("select error")
+            }else{
+                if(result.length > 0){      //로그인 성공
+                    req.session.login = result[0]
+                    res.redirect("/main")
+                }else{
+                    res.redirect("/")
+                }
+            }
+        }
+    )
 })
 
 app.get("/signup", function(req, res){
@@ -57,7 +108,66 @@ app.post("/signup2", function(req, res){
     var wallet = req.body._wallet
     console.log(id, pass, wallet)
     // mysql data insert
-    res.send(id, pass, wallet)
+    connection.query(
+        `insert into user_list values (?,?,?)`,
+        [id, pass, wallet],
+        function(err){
+            if(err){
+                console.log(err)
+                res.send("signup insert error")
+            }else{
+                res.redirect("/")
+            }
+        }
+    )
+})
+
+app.get("/add_car", function(req, res){
+    if(!req.session.login){
+        res.redirect("/")
+    }else{
+        res.render("add_car.ejs")
+    }
+})
+
+app.post("/add_car2", function(req, res){
+    var num = req.body._num
+    var year = req.body._year
+    var type = req.body._type
+    console.log(num, year, type)
+    // mysql insert
+    connection.query(
+        `insert into car_list(num, year, type) values (?,?,?)`,
+        [num, year, type], 
+        function(err){
+            if(err){
+                console.log(err)
+                res.send("add_car insert error")
+            }else{
+                res.redirect("/main")
+            }
+        }
+    )
+})
+
+app.get("/list", function(req, res){
+    if(!req.session.login){
+        res.redirect("/")
+    }else{
+        connection.query(
+            `select * from car_list`, 
+            function(err, result){
+                if(err){
+                    console.log(err)
+                    res.send("list select error")
+                }else{
+                    res.render("car_list.ejs", {
+                        list : result
+                    })
+                }
+            }
+        )
+    }
 })
 
 app.get("/trans", function(req, res){
